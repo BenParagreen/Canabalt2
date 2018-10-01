1d0102d73fc9df0781e39bdad7e7cb20bc3ae8ae
BenParagreen <43142130+BenParagreen@users.noreply.github.com>
2018-10-01 16:56:13 +0100

    Fixed

---
+++ Player.h
@@ -0,0 +1,28 @@
+#pragma once
+//Library Includes
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+//Project Includes
+#include "AnimationSystem.h"
+
+class Player
+{
+
+public:
+	Player();
+	void Input(sf::Event _gameEvent);
+	void Update(sf::Time _FrameTime);
+	void Draw(sf::RenderTarget _target);
+private:
+
+	// Assets
+	sf::Sprite m_sprite;
+	sf::Sound m_jumpSound;
+	sf::Sound m_deathSound;
+	sf::Sound m_landSound;
+	AnimationSystem m_animation;
+
+	//State
+	sf::Vector2f m_velocity;
+};
